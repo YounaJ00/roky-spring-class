874250c6bdc16a2b8d559b9519cf78d8298e0062
Youna Joo <you.na.joo0@gmail.com>
2026-08-03 14:17:29 +0900

    feat(post-domain): 게시물 도메인 및 유스케이스 포트 구현

---
+++ src/main/java/com/example/springboot/post/application/port/in/PostUseCase.java
@@ -0,0 +1,41 @@
+package com.example.springboot.post.application.port.in;
+
+import java.util.List;
+
+public interface PostUseCase {
+
+    PostResult create(
+            Long userId,
+            PostCommand command
+    );
+
+    PostResult get(Long postId);
+
+    List<PostResult> getAll();
+
+    PostResult update(
+            Long userId,
+            Long postId,
+            PostCommand command
+    );
+
+    void delete(
+            Long userId,
+            Long postId
+    );
+
+    record PostCommand(
+            String title,
+            String content
+    ) {
+    }
+
+    record PostResult(
+            Long id,
+            Long authorId,
+            String title,
+            String content,
+            long viewCount
+    ) {
+    }
+}
---
+++ src/main/java/com/example/springboot/post/domain/Post.java
@@ -0,0 +1,70 @@
+package com.example.springboot.post.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Post {
+
+    private final Long id;
+    private final Long authorId;
+
+    private String title;
+    private String content;
+    private long viewCount;
+
+    private Post(
+            Long id,
+            Long authorId,
+            String title,
+            String content,
+            long viewCount
+    ) {
+        this.id = id;
+        this.authorId = authorId;
+        this.title = title;
+        this.content = content;
+        this.viewCount = viewCount;
+    }
+
+    public static Post create(
+            Long authorId,
+            String title,
+            String content
+    ) {
+        return new Post(
+                null,
+                authorId,
+                title,
+                content,
+                0L
+        );
+    }
+
+    public static Post restore(
+            Long id,
+            Long authorId,
+            String title,
+            String content,
+            long viewCount
+    ) {
+        return new Post(
+                id,
+                authorId,
+                title,
+                content,
+                viewCount
+        );
+    }
+
+    public void update(
+            String title,
+            String content
+    ) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void increaseViewCount() {
+        viewCount++;
+    }
+}
